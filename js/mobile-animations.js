60928ea6048eac140f77b799d4a840aa56fe9ef5
thodorists <thodoris.stefanis@outlook.com>
2025-02-02 14:57:02 +0200

    added animations on mobile devices

---
+++ js/mobile-animations.js
@@ -0,0 +1,19 @@
+const mobile = window.matchMedia("(max-width: 1024px)");
+
+if(mobile.matches){
+    window.onload = function() {
+        const title = document.querySelector('.title');
+        title.style.transition = '2s'
+        title.style.transform = 'scale(100%)';
+    
+        const home = document.querySelector('.homepage');
+        home.style.transition = '2s'
+        home.style.transform = 'scale(100%)';
+
+        const homebuttons = document.querySelector('.homebuttons');
+        setTimeout(()=>{
+            homebuttons.style.transition = '2s'
+            homebuttons.style.transform = 'scale(100%)';
+        },500)
+    }
+}
