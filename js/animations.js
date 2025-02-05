6bf5f255cbc943c9e83e07bb8308ed15519b4075
thodorists <thodoris.stefanis@outlook.com>
2025-02-05 11:04:55 +0200

    better animation on buttons on site load

--- js/animations.js
+++ js/animations.js
@@ -9,10 +9,28 @@ if (laptop.matches){
         const home = document.querySelector('.home');
         home.style.transition = '2s'
         home.style.transform = 'scale(100%)';
-    
-        const homebuttons2 = document.querySelector('.homebuttons2');
-        homebuttons2.style.transform = 'scale(100%)'
-    
+
+        const seeproject2 = document.querySelector('.seeproject2');
+        seeproject2.style.display = 'none'
+        const contactMe2 = document.querySelector('.contactMe2');
+        contactMe2.style.display = 'none'
+
+        setTimeout(function(){
+            const seeproject2 = document.querySelector('.seeproject2');
+            seeproject2.style.display = 'flex'
+            const contactMe2 = document.querySelector('.contactMe2');
+            contactMe2.style.display = 'flex'
+        },1500)
+
+        setTimeout(function(){
+            const seeproject2 = document.querySelector('.seeproject2');
+            seeproject2.style.display = 'flex'
+            const contactMe2 = document.querySelector('.contactMe2');
+            seeproject2.style.display = 'flex'
+            seeproject2.style.transform = 'scale(100%)'
+            contactMe2.style.transform = 'scale(100%)'
+        },2000)
+
         setTimeout(function(){
             const header = document.querySelector('.header');
             header.style.transition = '1s';
